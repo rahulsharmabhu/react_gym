67bf9baa7145e3d4726e856a7567b3852a86ed2b
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-08-15 15:32:46 +0530

    done till 54 mins

--- src/components/SearchExcercises.js
+++ src/components/SearchExcercises.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React ,{useEffect,useState} from 'react'
+import {Box,Button,Stack,TextField, Typography} from '@mui/material';
+import { excerciseOptions,fetchData } from '../utils/fetchData';
 
 const SearchExcercises = () => {
+  const [search,setSearch] = useState('');
+  const handleSearch = async () => {
+    if(search){
+      const excercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',excerciseOptions);
+      console.log('this is the excercise data',excercisesData);
+    }
+  }
+
   return (
-    <div>SearchExcercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize:{lg: '44px',xs: '30px' }}} mb="50px" textAlign="center">
+        Awesome Excercises You <br/>
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+       <TextField 
+        sx={{
+        input: {
+        fontWeight:'700',
+        border:'none',
+        borderRadius:'4px'
+        },
+        width: {
+        lg: '800px',
+        xs:'350px',
+        backgroundColor:"#fff",
+        borderRadius:'40px'
+        }}} height="76px" value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())} placeholder="Search Excercises" type="text" />
+        <Button className="search-btn"
+        sx={{
+          bgcolor:'#FF2625',
+          color:'#fff',
+          textTransform:'none',
+          width:{lg:'175px',xs:'80px'},
+          fontSize:{lg:'20px',xs:'14px'},
+          height:'56px',
+          position: "absolute",
+          right: '0'
+        }}
+        onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const excerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': 'b597147930msh9501ed79294994ep141843jsn6d62b69d1be8'
+    }
+  };
+
+export const fetchData = async (url,options) =>{
+    const response = await fetch(url,options);
+    const data = await response.json();
+    return data;
+}
